bc75f2b83f3e84226342389dbd17bedb45d4ecea
NickDayFSD <80706339+NickDayFSD@users.noreply.github.com>
2021-03-30 15:42:03 -0700

    completed utils function

--- utils.js
+++ utils.js
@@ -19,4 +19,19 @@ export function createMonsterListing() {
     const image = document.createElement('img');
 
     image.src = monsters.image;
+
+    const pDescription = document.createElement('p');
+
+    pDescription.classList.add('description');
+    pDescription.textContent = monsters.description;
+
+    const pTamed = document.createElement('p');
+
+    pTamed.classList.add('is-tamed');
+    pTamed.textContent = monsters.isTamed;
+
+    const pPrice = document.createElement('p');
+
+    pPrice.classList.add('price');
+    pPrice.textContent = monsters.price;
 }
